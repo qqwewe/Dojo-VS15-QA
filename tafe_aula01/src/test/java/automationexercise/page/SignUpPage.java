ca5a273177deb12429d6af201e0621e66bb5d782
Arley Castro <arleycodeqa@gmail.com>
2025-01-30 14:14:06 -0400

    Adicionar seletores

--- tafe_aula01/src/test/java/automationexercise/page/SignUpPage.java
+++ tafe_aula01/src/test/java/automationexercise/page/SignUpPage.java
@@ -5,9 +5,11 @@ import org.openqa.selenium.By;
 public class SignUpPage {
 
     private static final By SIGNUP_HOME = By.cssSelector("div.header_user_info > a");
-
-
-
+    private static final By CREATE_ACOUNT = By.cssSelector("#SubmitCreate");
+    private static final By CREATE_EMAIL = By.cssSelector("#email_create");
+    private static final By CHECKBOX_MR = By.cssSelector("#id_gender1");
+    private static final By CHECKBOX_MRS = By.cssSelector("#id_gender2");
+    private static final By NAME = By.cssSelector("#customer_firstname");
 
 
 }
