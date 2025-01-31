5e161a52c684ef8110196197721e0c45e41bb581
Kevin Ribas <50385593+kevinribas@users.noreply.github.com>
2025-01-31 14:03:57 -0300

    feat: Login Test

--- tafe_aula01/src/test/java/automationexercise/page/SignInPage.java
+++ tafe_aula01/src/test/java/automationexercise/page/SignInPage.java
@@ -1,8 +1,39 @@
 package automationexercise.page;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.openqa.selenium.By;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SignInPage extends BasePage {
 
-    public static final By
+    public static final By MESSAGE_AUTH_PAGE = By.cssSelector("#center_column > h1");
+    public static final By INPUT_EMAIL = By.cssSelector("#email");
+    public static final By INPUT_PASSWORD = By.cssSelector("#passwd");
+    public static final By BUTTON_LOGIN = By.cssSelector("#SubmitLogin > span");
+    private static final By SIGNUP_HOME = By.cssSelector("div.header_user_info > a");
+
+    public void fillEmail(String email) {
+        preencherInput(INPUT_EMAIL, email);
+    }
+
+    public void fillPassword(String password) {
+        preencherInput(INPUT_PASSWORD, password);
+    }
+
+    public void clickLoginButton() {
+        clicar(BUTTON_LOGIN);
+    }
+
+    public String verifyAuthPage() {
+        return lerTexto(MESSAGE_AUTH_PAGE);
+    }
+
+    public void clickSignUpHomeButton() {
+        clicar(SIGNUP_HOME);
+    }
+
 }
---
+++ tafe_aula01/src/test/java/automationexercise/test/SignInTest.java
@@ -0,0 +1,24 @@
+package automationexercise.test;
+
+import automationexercise.data.UserDto;
+import automationexercise.page.SignInPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static automationexercise.factory.data.SignInData.createSignInDto;
+
+public class SignInTest extends BaseTest {
+    SignInPage signInPage = new SignInPage();
+
+    @Test
+    public void testSignInValidUser() {
+        UserDto user = createSignInDto();
+        signInPage.clickSignUpHomeButton();
+        String msg = signInPage.verifyAuthPage();
+        Assert.assertEquals(msg);
+
+        signInPage.fillEmail(user.getEmail());
+        signInPage.fillPassword(user.getPassword());
+        signInPage.clickLoginButton();
+    }
+}
