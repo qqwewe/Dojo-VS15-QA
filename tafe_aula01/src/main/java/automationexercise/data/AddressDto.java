2b4079d0394f95330f27611188a6f1e089cc2a8e
Luan Prudente <luanprudente.dev@gmail.com>
2025-01-31 14:53:00 -0300

    page Address

--- tafe_aula01/src/main/java/automationexercise/data/AddressDto.java
+++ tafe_aula01/src/main/java/automationexercise/data/AddressDto.java
@@ -2,5 +2,11 @@ package automationexercise.data;
 
 public class AddressDto {
 
-    private String
+    private String adrress;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String phoneHome;
+    private String phoneMobile;
+
 }
--- tafe_aula01/src/test/java/automationexercise/page/AddressPage.java
+++ tafe_aula01/src/test/java/automationexercise/page/AddressPage.java
@@ -1,4 +1,16 @@
 package automationexercise.page;
 
+import org.openqa.selenium.By;
+
 public class AddressPage extends BasePage{
+
+    public static final By address = By.cssSelector("#address1");
+    public static final By city = By.cssSelector("#city");
+    public static final By state = By.cssSelector("#id_state");
+    public static final By zipCode = By.cssSelector("#postcode");
+    public static final By phoneHome = By.cssSelector("#phone");
+    public static final By phoneMobile = By.cssSelector("#phone_mobile");
+    public static final By buttonSave = By.cssSelector("#submitAddress > span");
+
+    public void fillAdrres(String address1){preencherInput(address, address1);}
 }
